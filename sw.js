3f373b50b3dc43d3db2d4bf6516b9e091b3205ff
Dominik <dhuesener@gmail.com>
2022-03-07 10:37:09 +0100

    fix minor glitches

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('geldmaschine-store').then((cache) => cache.addAll([
-        'client/index.html'
+        //'client/index.html'
       ])),
     );
   });
   
-  self.addEventListener('fetch', (e) => {
+  /*self.addEventListener('fetch', (e) => {
     //console.log(e.request.url);
     e.respondWith(
       caches.match(e.request).then((response) => response || fetch(e.request)),
     );
-  });
+  });*/
   
